302d8031cc2fd002cd1a385ace57b0e1302bf7d8
Moaad <Mgountetti27@outlook.com>
2024-02-18 22:56:40 +0000

    refreshed login and signup, added sign up functionality

--- edudev/app/login/page.js
+++ edudev/app/login/page.js
@@ -1,6 +1,64 @@
-export default function Login() {
+'use client'
+import React, { useState } from "react";
+import { auth } from "@/app/firebase";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      // You can redirect to another page or handle the login success in another way
+      console.log("Login successful!");
+    } catch (error) {
+      console.error("Error during login:", error.message);
+    }
+  };
     return (
-      <h1 class= "tex-white">waawaa</h1>
-    )
-  }
+        <div className="flex items-center justify-center h-screen">
+            <form className="bg-gray-800 p-8 shadow-md rounded-md border border-slate-700" onSubmit={handleLogin}>
+            <h2 className="text-2xl font-bold mb-4">Login</h2>
+            <div className="mb-4">
+                <label className="block text-gray-300 text-sm font-bold mb-2" htmlFor="email">
+                Email:
+                </label>
+                <input
+                type="email"
+                id="email"
+                className="w-full p-2 border rounded-md focus:text-black"
+                placeholder="example@company.com"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                />
+            </div>
+            <div className="mb-4">
+                <label className="block text-gray-300 text-sm font-bold mb-2 focus:text-gray-800" htmlFor="password">
+                Password:
+                </label>
+                <input
+                type="password"
+                id="password"
+                className="w-full p-2 border focus:text-black rounded-md"
+                placeholder="*******"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+            </div>
+            <button
+                type="submit"
+                className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
+            >
+                Login
+            </button>
+            <div className="py-2">
+                <a href="/sign-up" className="text-gray-300 text-align:center text-sm hover:text-blue-300">don't have an account?</a>
+            </div>
+            </form>
+        </div>
+        );
+    };
   
+  
+export default Login;
