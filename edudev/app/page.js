285d809ef0be95c0c143dfb5e566037dd82b242c
Moaad <Mgountetti27@outlook.com>
2024-04-22 12:09:12 +0100

    sorted news screen

--- edudev/app/page.js
+++ edudev/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 import { useState , useEffect } from 'react';
 import axios from 'axios';
-import { Card , CardMedia , CardContent , Typography , Button  , Pagination , Container, CardActionArea } from '@mui/material';
+import { Card , CardMedia , CardContent , Typography , Button  , Pagination , Container, CardActionArea , Divider } from '@mui/material';
 import { useRouter } from "next/navigation";
 
 
@@ -38,23 +38,34 @@ export default function Home() {
     const article = articleList[currentArticle];
     return (
       <Container>
-        <div className='grid grid-cols-2 items-center h-screen'>
-          <div className='mx-auto'> 1 </div>
-          <div className='mx-auto'>
-            <Card className='w-1/2'>
-              <CardActionArea onClick={()=>router.push(article.url)}>
-              <img className='h-40 w-full' src={article.urlToImage} />
-              <CardContent className='overflow-hidden'>
+        <div className='grid grid-cols-2 h-screen mt-10'>
+          <div className=''> 1 </div>
+          <div className=' flex-col justify-between'>
+            <div className='mb-10'>
+            <Typography level="h2" className="text-xl font-bold mb-3">NEWS</Typography>
+                <Card elevation={10} className='w-full h-1/2'>
+                  <CardActionArea onClick={()=>router.push(article.url)}>
+                  <img className='h-40 w-full' src={article.urlToImage} />
+                  <CardContent className='overflow-hidden'>
+                    <Typography className="truncate" variant="h5">
+                      {article.title}
+                    </Typography>
+                    <Typography className="" variant="body2" color="text.secondary">
+                      {article.description}
+                    </Typography>
+                  </CardContent>
+                  </CardActionArea>
+                </Card>
+              <div className='flex justify-center mt-4'>
+              <Pagination count={5} size="large" onChange={(e, page) => setCurrentArticle(page - 1)} />
+              </div>
+            </div>
+            <div className='mt-10'>
+              <Typography level="h1" className="text-xl font-bold mb-3">Topic Of the Week</Typography>
                 <Typography className="truncate" variant="h5">
-                  {article.title}
+                    Continuous Integration
                 </Typography>
-                <Typography className="" variant="body2" color="text.secondary">
-                  {article.description}
-                </Typography>
-              </CardContent>
-              </CardActionArea>
-            </Card>
-            <Pagination count={5} size="large" onChange={(e, page) => setCurrentArticle(page - 1)} />
+            </div>
           </div>
         </div> 
       </Container> 
